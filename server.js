769fa41fc5aee512ba3ddb79bf784137e49dcac6
hoangphuccamila-svg <hoangphuc.camila@gmail.com>
2026-09-18 17:49:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const DATA=path.join(__dirname,'data.json');
 const prizes=[
  {name:'Bao cơm trưa',weight:0.5},
  {name:'Cà phê',weight:0.5},
- {name:'10.000 VND',weight:10},
+ {name:'10.000 VND',weight:20},
  {name:'20.000 VND',weight:6},
- {name:'50.000 VND',weight:8},
- {name:'100.000 VND',weight:2},
+ {name:'50.000 VND',weight:10},
+ {name:'100.000 VND',weight:1},
  {name:'200.000 VND',weight:1},
- {name:'Trúng Gió',weight:74}
+ {name:'Trúng Gió',weight:60}
 ];
 if(!fs.existsSync(DATA))fs.writeFileSync(DATA,JSON.stringify({players:{},history:[]},null,2));
 const read=()=>JSON.parse(fs.readFileSync(DATA,'utf8'));
